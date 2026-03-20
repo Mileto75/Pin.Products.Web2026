9f821963490e509a71b138c7a2c6e4dfcc67bdb3
Mileto Di Marco <mileto.di.marco@howest.be>
2026-03-20 09:38:35 +0100

    Add project files.

---
+++ Pin.Products.Core/Services/CategoryApiService.cs
@@ -0,0 +1,37 @@
+﻿using Pin.Products.Core.Services.Interfaces;
+using Pin.Products.Core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Pin.Products.Core.Services
+{
+    public class CategoryApiService : ICategoryApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoryApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://api.escuelajs.co/api/v1/");
+        }
+
+        public async Task<ResultModel<IEnumerable<CategoryModel>>> GetAllAsync()
+        {
+            var resultModel = new ResultModel<IEnumerable<CategoryModel>>();
+            var result = await _httpClient.GetAsync($"{_httpClient.BaseAddress}categories");
+            if(!result.IsSuccessStatusCode)
+            {
+                resultModel.Errors = new List<string> { "Connection error!" };
+                return resultModel;
+            }
+            var content = await result.Content.ReadAsStringAsync();
+            resultModel.Data = JsonSerializer.Deserialize<IEnumerable<CategoryModel>>(content);
+            return resultModel;
+        }
+    }
+}
---
+++ Pin.Products.Core/Services/Interfaces/ICategoryApiService.cs
@@ -0,0 +1,14 @@
+﻿using Pin.Products.Core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.Products.Core.Services.Interfaces
+{
+    public interface ICategoryApiService
+    {
+        Task<ResultModel<IEnumerable<CategoryModel>>> GetAllAsync();
+    }
+}
