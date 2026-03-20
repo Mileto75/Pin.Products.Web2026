8cd9b80908fe07702b2d1122dad13a7458853828
Mileto Di Marco <mileto.di.marco@howest.be>
2026-03-20 11:10:06 +0100

    Add createOrUpdateCategoryModel

---
+++ Pin.Products.Core/Services/Models/CreateOrUpdateCategoryModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Pin.Products.Core.Services.Models
+{
+    public class CreateOrUpdateCategoryModel
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
+    }
+}
