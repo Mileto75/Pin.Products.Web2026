58953a169a805f3275ece207cda6cd0da5c03acc
Mileto Di Marco <mileto.di.marco@howest.be>
2026-04-01 16:10:15 +0200

    Add ProductService implementation

---
+++ Pin.Products.Core/Services/ProductService.cs
@@ -0,0 +1,41 @@
+﻿using Pin.Products.Core.Services.Interfaces;
+using Pin.Products.Core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.Products.Core.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://api.escuelajs.co/api/v1/products");
+        }
+
+        public Task<ResultModel<ProductModel>> CreateAsync(CreateOrUpdateProductModel newProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<IEnumerable<ProductModel>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<ProductModel>> UpdateAsync(CreateOrUpdateProductModel newProduct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
